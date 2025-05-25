753718afb1bc6fc22db4355fa71df4bdf01dc1d2
kiper22 <kac.vielebny@gmail.com>
2025-05-25 21:33:39 +0200

    Remote interface - hotel service

--- hotelService/HotelService.java
+++ hotelService/HotelService.java
@@ -1,8 +1,20 @@
+// Co może wywołać klient zdalnie na serwerze
 package hotelService;
 
 import java.rmi.Remote;
 import java.rmi.RemoteException;
+import java.util.Date;
+import java.util.List;
 
-public interface HotelService extends Remote{
+public interface HotelService extends Remote {
+    // na podstawie albo daty albo lokalizacji albo obu
+    List<Room> getAvailableRooms() throws RemoteException;
     
+    boolean bookRoom(int roomId, String name, String surname, String phoneNumber, Date start, Date stop) throws RemoteException;
+
+    boolean cancelReservation(int reservationId, String name, String surname, String phoneNumber) throws RemoteException;
+
+    // moje rezerwacje
+    List<Reservation> getReservations() throws RemoteException;
+
 }
