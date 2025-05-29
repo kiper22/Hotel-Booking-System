94b8780a477e1d7447d2dc6d669d05b5a5ca7572
Dawid <dawid.topyla11@gmail.com>
2025-05-29 14:58:04 +0200

    Implementacja klas Hotel oraz Room wraz z metodami.

---
+++ hotelService/enums/RoomType.java
@@ -0,0 +1,14 @@
+package hotelService.enums;
+
+public enum RoomType {
+    /*
+        SINGLE_BED -> pokój dla jednej osoby
+        DOUBLE_BED -> pokój dwuosobowy z 2 pojedyńczymi lub 1 podwójnym łóżkiem
+        TRIPLE_BED -> pokój dla 3 osób
+        FAMILY     -> pokój dla 4 do 6 osób
+     */
+    SINGLE_BED,
+    DOUBLE_BED,
+    TRIPLE_BED,
+    FAMILY,
+}
